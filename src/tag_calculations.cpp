917f5a6299cc40f0c576ff33c9f0ce4fd3b0008a
Patrick Wang <zonghan2000@gmail.com>
2017-07-20 16:15:34 -0400

    published tagdata to topic for rqt_plot

--- src/tag_calculations.cpp
+++ src/tag_calculations.cpp
@@ -9,10 +9,6 @@
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/image_encodings.h>
 #include <boost/foreach.hpp>
-#include <geometry_msgs/PoseStamped.h>
-#include <geometry_msgs/PoseArray.h>
-#include </home/pat/it_ws/devel/.private/apriltags_ros/include/apriltags_ros/AprilTagDetection.h>
-#include </home/pat/it_ws/devel/.private/apriltags_ros/include/apriltags_ros/AprilTagDetectionArray.h>
 #include <AprilTags/Tag36h11.h>
 
 #include <math.h>
@@ -25,6 +21,8 @@
 #define cx 427.522877
 #define cy 312.683933
 
+#include <bu_project/tagdata.h>
+
 //for total over time average
 struct average{
   Eigen::Vector3d mDistance;
@@ -33,6 +31,7 @@ struct average{
 };
 
 average averages[5];
+ros::Publisher pub;
 
 void imageCallback(const sensor_msgs::ImageConstPtr& msg){
   cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
@@ -51,7 +50,7 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msg){
   for (int i = 0; i < 5; i++)
     found[i] = 0;
 
-  //extracts data from tag detection
+  //extracts rotation and translation matrices, marks tags as found, converts rotation to 0 to 360 angle in degrees
   BOOST_FOREACH(AprilTags::TagDetection detection, detections){
     int di = detection.id;
     found[di] = 1; 
@@ -68,46 +67,58 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msg){
   }
 
   //prints data
-  for (int i = 0; i < 25; i++)
-    std::cout << '-';
+  /*for (int i = 0; i < 25; i++)
+    std::cout << '-';*/
   for (int i = 0; i < 5; i++){
-    if (found[i]){
-      std::cout << "\nid: " << i << "\n";
+    if (found[i]&&found[0]&&i!=0){
+     /* std::cout << "\nid: " << i << "\n";
       std::cout << "rot: \n" << rot[i] << std::endl;
       std::cout << "trans: \n" << trans[i] << "\n";
       std::cout << "xtheta: " << theta[i][0] << "\n";
       std::cout << "ytheta: " << theta[i][1] << "\n";
-      std::cout << "ztheta: " << theta[i][2] << "\n";
-	
+      std::cout << "ztheta: " << theta[i][2] << "\n";*/
+      /*
       //updates average over time for rotation and translation
       if (found[0]&&i!=0){
+	
+	//updates average over time for ratation and translation
 	averages[i].polls++;
         averages[i].mDistance = averages[i].mDistance*(averages[i].polls-1)/averages[i].polls+(trans[i]-trans[0])/averages[i].polls;
 	for (int j = 0; j < 3; j++)
 	  averages[i].mTheta[j] = averages[i].mTheta[j]*(averages[i].polls-1)/averages[i].polls+(theta[i][j]-theta[0][j])/averages[i].polls;
-	
-	std::cout << "distance from tag 0: \n" << trans[i]-trans[0] << "\n";
+	*/
+	/*std::cout << "distance from tag 0: \n" << trans[i]-trans[0] << "\n";
 	std::cout << "average distance from tag 0: \n" << averages[i].mDistance << "\n";
 	std::cout << "xtheta from tag 0: " << theta[i][0]-theta[0][0] << "\n";
 	std::cout << "average xtheta from tag 0: " << averages[i].mTheta[0] << "\n";
         std::cout << "ytheta from tag 0: " << theta[i][1]-theta[0][1] << "\n";
 	std::cout << "average ytheta from tag 0: " << averages[i].mTheta[1] << "\n";
 	std::cout << "ztheta from tag 0: " << theta[i][2]-theta[0][2] << "\n";
-        std::cout << "average ztheta from tag 0: " << averages[i].mTheta[2] << "\n";
-      }
+        std::cout << "average ztheta from tag 0: " << averages[i].mTheta[2] << "\n";*/
+        
+        //if tag other than tag 0 is detected as well as tag 0, publish 2d translation and y-axis rotation
+	bu_project::tagdata td;
+	td.id = i;
+	Eigen::Vector3d tempv = trans[i]-trans[0];
+	td.x = tempv(0);
+	td.y = tempv(1);
+	td.theta = theta[i][1]-theta[0][1];
+        pub.publish(td);
+      //}
 
-      std::cout << "\n\n";
+      //std::cout << "\n\n";
     }
   }
-  for (int i = 0; i < 25; i++)
-    std::cout << '-';
-  std::cout << "\n\n\n";
+  //for (int i = 0; i < 25; i++)
+  //  std::cout << '-';
+  //std::cout << "\n\n\n";
 
 }
 
 int main(int argc, char**argv){
   ros::init(argc, argv, "tag_calculations");
   ros::NodeHandle nh;
+  pub = nh.advertise<bu_project::tagdata>("tagdata", 1000);
   image_transport::ImageTransport it(nh);
   image_transport::Subscriber sub = it.subscribe("camera/image", 1, imageCallback);
   ros::spin();
